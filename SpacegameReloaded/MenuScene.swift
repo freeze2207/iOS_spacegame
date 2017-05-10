ecd2dbae9a36a34f1f5d06046512a802508d3678
freeze2207 <zhangzihezealf@gmail.com>
2017-05-10 18:40:48 -0400

    Initial
    Bug fixed:#initialization object

---
+++ SpacegameReloaded/MenuScene.swift
@@ -0,0 +1,84 @@
+//
+//  MenuScene.swift
+//  SpacegameReloaded
+//
+//  Created by ZhangZihe on 2017-02-07.
+//  Copyright © 2017 Training. All rights reserved.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene
+{
+    var starfield:SKEmitterNode!
+    
+    var newGameButtonNode:SKSpriteNode!
+    var difficultyButtonNode:SKSpriteNode!
+    var difficultyLabelNode:SKLabelNode!
+    
+    override func didMove(to view: SKView)
+    {
+        starfield = self.childNode(withName: "starfield") as! SKEmitterNode
+        starfield.advanceSimulationTime(10)
+        
+        newGameButtonNode = self.childNode(withName: "newGameButton") as! SKSpriteNode
+        difficultyButtonNode = self.childNode(withName: "difficultyButton") as! SKSpriteNode
+        
+        difficultyButtonNode.texture = SKTexture(imageNamed: "difficultyButton")
+        
+        difficultyLabelNode = self.childNode(withName: "difficultyLabel") as! SKLabelNode
+        
+        let userDefaults = UserDefaults.standard
+        if userDefaults.bool(forKey: "hard")
+        {
+            difficultyLabelNode.text = "Hard"
+        }
+        else
+        {
+            difficultyLabelNode.text = "Easy"
+        }
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        let touch = touches.first
+        
+        if let location = touch?.location(in: self) {
+            let nodesArray = self.nodes(at: location)
+            
+            if nodesArray.first?.name == "newGameButton" {
+                let transition = SKTransition.flipHorizontal(withDuration: 0.75)
+                let gameScene = GameScene(size: self.size)
+                
+                self.view?.presentScene(gameScene, transition: transition)
+                
+            }
+            else if nodesArray.first?.name == "difficultyButton"
+            {
+                changeDifficulty()
+            }
+        }
+    }
+    
+    func changeDifficulty()
+    {
+        let userDefaults = UserDefaults.standard
+        
+        if difficultyLabelNode.text == "Easy"
+        {
+            difficultyLabelNode.text = "Hard"
+            userDefaults.set(true, forKey: "hard")
+        }
+        else
+        {
+            difficultyLabelNode.text = "Easy"
+            userDefaults.set(false, forKey: "hard")
+        }
+        
+        userDefaults.synchronize()
+        
+    }
+    
+    
+}
